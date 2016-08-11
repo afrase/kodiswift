207a7436b1d03adf53a99fb7e3e1ce037717b676
Vladimir Kravets <vova.kravets@gmail.com>
2016-08-11 23:39:56 +0300

    Add xbmcvfs mock

--- kodiswift/mockxbmc/xbmcvfs.py
+++ kodiswift/mockxbmc/xbmcvfs.py
@@ -1,1 +1,20 @@
 # -*- coding: utf-8 -*-
+
+import os
+
+
+def exists(target):
+    return os.path.exists(target)
+
+
+def rename(origin, target):
+    return os.rename(origin, target)
+
+
+def delete(target):
+    if os.path.isfile(target) and not os.path.isdir(target):
+        return os.unlink(target)
+    return False
+
+def mkdir(target):
+    os.mkdir(target)
