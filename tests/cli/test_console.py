4a46336aa243f7a261654d312afbf65e2fb0b35b
Aaron Frase <afrase91@gmail.com>
2017-03-28 16:05:23 -0400

    Fixed test_console and made it more readable

--- tests/cli/test_console.py
+++ tests/cli/test_console.py
@@ -13,43 +13,76 @@ class TestConsole(unittest.TestCase):
         self.assertEqual(console.get_max_len([]), 0)
 
     def test_display_video(self):
-        items = [{'label': 'X', 'path': 'Y'}, {'label': 'Z', 'path': 'Q'} ]
+        items = [
+            {'label': 'X', 'path': 'Y'},
+            {'label': 'Z', 'path': 'Q'}
+        ]
         list_items = [ListItem.from_dict(**item) for item in items]
+
         with patch('sys.stdout', new=StringIO.StringIO()) as stdout:
-            console.display_video( list_items )
+            console.display_video(list_items)
 
         output = stdout.getvalue().strip()
-        self.assertEquals(output,'-------------------\nPlaying Media Z (Q)\n-------------------\n[0] X (Y)')
+        self.assertEquals(output, '-------------------\n'
+                                  'Playing Media Z (Q)\n'
+                                  '-------------------\n'
+                                  '[0] X (Y)')
 
     def test_display_video_form_listitems(self):
-        items = [{'label': '..', 'path': 'Y'}, {'label': 'Z', 'path': 'Q', 'played': True} ]
+        items = [
+            {'label': '..', 'path': 'Y'},
+            {'label': 'Z', 'path': 'Q', 'played': True}
+        ]
         list_items = [ListItem.from_dict(**item) for item in items]
         list_items[1].set_played(True)
+
         with patch('sys.stdout', new=StringIO.StringIO()) as stdout:
-            console.display_listitems( list_items, "PARENT_URL" )
+            console.display_listitems(list_items, "PARENT_URL")
 
         output = stdout.getvalue().strip()
-        self.assertEquals(output,'-------------------\nPlaying Media Z (Q)\n-------------------\n[0] .. (Y)')
+        self.assertEquals(output, '-------------------\n'
+                                  'Playing Media Z (Q)\n'
+                                  '-------------------\n'
+                                  '[0] .. (Y)')
 
     def test_display_listitems_with_single_item(self):
-        items = [{'label': '..', 'path': 'Y'}, {'label': 'Z', 'path': 'Q'} ]
+        items = [
+            {'label': '..', 'path': 'Y'},
+            {'label': 'Z', 'path': 'Q'}
+        ]
         list_items = [ListItem.from_dict(**item) for item in items]
+
         with patch('sys.stdout', new=StringIO.StringIO()) as stdout:
-            console.display_listitems( list_items,'PARENT_URL' )
+            console.display_listitems(list_items, 'PARENT_URL')
 
         output = stdout.getvalue().strip()
-        self.assertEquals(output, '===========\nCurrent URL: PARENT_URL\n-----------\n #  Label Path\n-----------\n[0] .. (..)\n[1] Z  (Z)\n-----------')
+        self.assertEquals(output, '==========\n'
+                                  'Current URL: PARENT_URL\n'
+                                  '----------\n'
+                                  ' #  Label Path\n'
+                                  '----------\n'
+                                  '[0] .. (Y)\n'
+                                  '[1] Z  (Q)\n'
+                                  '----------')
 
     def test_display_listitems_with_multiple_items(self):
         items = [
-            {'label': 'A_LABEL', 'path': 'B_PATH'},
+            {'label': 'A_LABEL', 'path': 'A_PATH'},
             {'label': 'B_LABEL', 'path': 'B_PATH'},
             {'label': 'C_LABEL', 'path': 'C_PATH'}
-            ]
+        ]
         list_items = [ListItem.from_dict(**item) for item in items]
 
         with patch('sys.stdout', new=StringIO.StringIO()) as stdout:
-            console.display_listitems( list_items,'PARENT_URL' )
+            console.display_listitems(list_items, 'PARENT_URL')
 
         output = stdout.getvalue().strip()
-        self.assertEquals(output,'=====================\nCurrent URL: PARENT_URL\n---------------------\n #  Label   Path\n---------------------\n[0] A_LABEL (A_LABEL)\n[1] B_LABEL (B_LABEL)\n[2] C_LABEL (C_LABEL)\n---------------------')
+        self.assertEquals(output, '====================\n'
+                                  'Current URL: PARENT_URL\n'
+                                  '--------------------\n'
+                                  ' #  Label   Path\n'
+                                  '--------------------\n'
+                                  '[0] A_LABEL (A_PATH)\n'
+                                  '[1] B_LABEL (B_PATH)\n'
+                                  '[2] C_LABEL (C_PATH)\n'
+                                  '--------------------')
